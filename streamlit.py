28c79ecf0f49349cd3063a70e47fa0e45fea0197
aadil-704 <111413328+aadil-704@users.noreply.github.com>
2024-04-29 18:41:04 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,15 +2,12 @@ import xgboost as xgb
 import streamlit as st
 import pandas as pd
 
-#Loading up the Regression model we created
+# Loading up the Regression model we created
 model = xgb.XGBRegressor()
 model.load_model('xgb_model.json')
 
-#Caching the model for faster loading
+# Caching the model for faster loading
 @st.cache
-
-
-# Define the prediction function
 def predict(carat, cut, color, clarity, depth, table, x, y, z):
     #Predicting the price of the carat
     if cut == 'Fair':
@@ -55,14 +52,12 @@ def predict(carat, cut, color, clarity, depth, table, x, y, z):
         clarity = 6
     elif clarity == 'IF':
         clarity = 7
-    
 
     prediction = model.predict(pd.DataFrame([[carat, cut, color, clarity, depth, table, x, y, z]], columns=['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']))
     return prediction
 
-
 st.title('Diamond Price Predictor')
-st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
+st.image("https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png")
 st.header('Enter the characteristics of the diamond:')
 carat = st.number_input('Carat Weight:', min_value=0.1, max_value=10.0, value=1.0)
 cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
@@ -74,6 +69,9 @@ x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0,
 y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
 z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
 
+exchange_rate_usd_to_inr = 75  # Conversion rate from USD to INR
+
 if st.button('Predict Price'):
-    price = predict(carat, cut, color, clarity, depth, table, x, y, z)
-    st.success(f'The predicted price of the diamond is ${price[0]:.2f} USD')
+    price_in_usd = predict(carat, cut, color, clarity, depth, table, x, y, z)
+    price_in_inr = price_in_usd * exchange_rate_usd_to_inr
+    st.success(f'The predicted price of the diamond is ₹{price_in_inr:.2f} INR')
